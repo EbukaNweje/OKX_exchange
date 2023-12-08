a669f6145ed84eeb4322e3669d2171cc7dc79097
eflexTech <eflexmedia1@gmail.com>
2023-12-08 02:03:30 -0800

    AA

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
 import {HashRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPages';
 import Signup from './components/Auth/SignUp';
